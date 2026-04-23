909addcfe5d40af66c24df185f9896a6865c13e6
Kaio <kaiomascarenhas241@gmail.com>
2026-04-22 22:09:49 -0300

    Salvando conteúdo da aula 22/04/2026
    Tipos de Comentários
    Variaveis
    Constantes
    Array

---
+++ 04_variaveis.php
@@ -0,0 +1,33 @@
+<?php
+$name = 'Carlos Ferreira';
+echo $name;
+echo '<br>';
+
+$name = 'João Carlos';
+echo $name;
+echo '<br>';
+
+$name = 12.2;
+echo $name;
+echo '<br>';
+
+$name = 123;
+echo $name;
+echo '<br>';
+
+$name = 'Ariana Grande';
+echo $name;
+echo '<br>';
+
+$name = 'Doja Cat';
+echo $name;
+echo '<br>';
+
+$name = 'Cobrah';
+echo $name;
+echo '<br>';
+
+$name = 'AKOSIA';
+echo $name;
+echo '<br>';
+?>
---
+++ 06_array.php
@@ -0,0 +1,16 @@
+<?php
+//$carros = ['Uno' , 'Gol', 'Fusca', 'Civic'];
+//echo $carros[2];
+
+//$carroUm = ['Uno', 'Fiat', 'Branco'];
+//echo $carroUm[2];
+
+$carro_um = [
+  'modelo' => 'Uno',
+  'marca' => 'Fiat',
+  'cor' => 'Branco',
+];
+echo $carro_um['cor'];
+echo $carro_um['modelo'];
+echo $carro_um['marca'];
+?>
