a3c8739f72cc55ea6ea2b1c51f8c96d36b3f10ae
Kaio <kaiomascarenhas241@gmail.com>
2026-04-27 20:14:17 -0300

    trabalhando com arrays

--- 06_array.php
+++ 06_array.php
@@ -10,7 +10,52 @@ $carro_um = [
   'marca' => 'Fiat',
   'cor' => 'Branco',
 ];
+
 echo $carro_um['cor'];
+echo ' - ';
 echo $carro_um['modelo'];
+echo ' - ';
 echo $carro_um['marca'];
+
+$carro_dois = [
+  'modelo' => 'Pulse',
+  'marca' => 'Fiat',
+  'cor' => 'Amarelo',
+];
+
+echo '<br>';
+echo $carro_dois['cor'];
+echo ' - ';
+echo $carro_dois['modelo'];
+echo ' - ';
+echo $carro_dois['marca'];
+
+$carro_tres = [
+  'modelo' => 'Titano',
+  'marca' => 'Fiat',
+  'cor' => 'Vermelho',
+];
+
+echo '<br>';
+echo $carro_tres['cor'];
+echo ' => ';
+echo $carro_tres['modelo'];
+echo ' => ';
+echo $carro_tres['marca'];
+echo '<br>';
+
+$carro_quatro = [
+  'modelo' => 'Ducato',
+  'marca' => 'Fiat',
+  'cor' => 'cinza',
+];
+
+echo 'modelo: ', $carro_quatro ['modelo'], '<br>';
+echo 'marca: ', $carro_quatro ['marca'], '<br>';
+echo 'cor: ', $carro_quatro ['cor'], '<br>';
+
+/*
+Lembrete: falta terminar ....
+*/
+
 ?>
