e6d04ad1bb466d36697a3fcd81b30492f4e579df
Kaio <kaiomascarenhas241@gmail.com>
2026-05-04 19:51:46 -0300

    Aula de concatenação

---
+++ 12_concatenacao.php
@@ -0,0 +1,21 @@
+<?php
+$var1 = 'Kaio';
+$var2 = 'Mascarenhas';
+
+//echo $var1 . ' ' . $var2 . ' '. 22
+
+$var3 = $var1 . ' '. $var2 . ' '. 22;
+//echo $var3;
+
+$nome = 'Rubens Pereira';
+$empresa = 'Senac Americana';
+
+//echo 'O ' .$nome. ' é fundador da Empresa: '.$empresa;
+
+//echo '<br>';
+
+//echo 'O ' .$nome. ' trabalhar na Empresa de tecnologia chamado de: '.$empresa;
+
+//echo "O {$nome} <br> é fundador da Empresa: {$empresa}";
+
+?>
